00bc236f701c83f4c950c9effea056cfebbc8b67
Ntokozo Gumede <ntandontokozo5@gmail.com>
2024-07-17 11:59:27 +0200

    initial commit

---
+++ src/assets/images/index.js
@@ -0,0 +1,55 @@
+import bigShoe1 from './big-shoe1.png'
+import bigShoe2 from './big-shoe2.png'
+import bigShoe3 from './big-shoe3.png'
+
+import customer1 from './customer1.jpeg'
+import customer2 from './customer2.svg'
+
+import footerLogo from './footer-logo.svg'
+import headerLogo from './header-logo.svg'
+
+import offer from './offer.svg'
+
+import shoe_1 from './shoe_1.png'
+import shoe_2 from './shoe_2.png'
+import shoe_3 from './shoe_3.png'
+import shoe_4 from './shoe_4.png'
+import shoe_5 from './shoe_5.png'
+import shoe_6 from './shoe_6.png'
+import shoe_7 from './shoe_7.png'
+import shoe_8 from './shoe_8.png'
+import shoe8 from './shoe8.svg'
+
+
+import thumbnailBackground from './thumbnail-background.svg'
+import thumbnailShoe1 from './thumbnail-shoe1.svg'
+import thumbnailShoe2 from './thumbnail-shoe2.svg'
+import thumbnailShoe3 from './thumbnail-shoe3.svg'
+
+export {
+    bigShoe1,
+    bigShoe2,
+    bigShoe3,
+
+    customer1,
+    customer2,
+
+    footerLogo,
+    headerLogo,
+
+    offer,
+    shoe_1,
+    shoe_2,
+    shoe_3,
+    shoe_4,
+    shoe_5,
+    shoe_6,
+    shoe_7,
+    shoe_8,
+    shoe8,
+
+    thumbnailBackground,
+    thumbnailShoe1,
+    thumbnailShoe2,
+    thumbnailShoe3
+}
